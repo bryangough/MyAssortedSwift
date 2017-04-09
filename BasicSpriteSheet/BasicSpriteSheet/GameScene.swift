590495ead54acbe507c5f823f99b043b130ded02
Bryan Gough <gough.bryan@gmail.com>
2017-04-09 16:16:15 -0400

    Trying out loading a very basic sprite sheet

---
+++ BasicSpriteSheet/BasicSpriteSheet/GameScene.swift
@@ -0,0 +1,38 @@
+//
+//  GameScene.swift
+//  BasicSpriteSheet
+//
+//  Created by Bryan Gough on 2017-04-09.
+//  Copyright © 2017 Bryan Gough. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    // 1
+   /* let sheet = BasicPeople()
+    var sprite:SKSpriteNode
+    
+    
+    override init(size: CGSize) {
+        sprite = SKSpriteNode(texture: sheet.movingPerson1_idle0001());
+        let walk = SKAction.animate(with: sheet.movingPerson1_walk(), timePerFrame: 0.033)
+        sprite.run(walk)
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func didMove(to view: SKView) {
+        // 2
+        backgroundColor = SKColor.white
+        // 3
+        sprite.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        // 4
+        addChild(sprite)
+    }*/
+}
---
+++ BasicSpriteSheet/BasicSpriteSheet/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  BasicSpriteSheet
+//
+//  Created by Bryan Gough on 2017-04-09.
+//  Copyright © 2017 Bryan Gough. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
